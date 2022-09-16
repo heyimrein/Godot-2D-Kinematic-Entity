e702e9df43df9380fe23802a67f4047f38721239
reinlmao <rekreingd@gmail.com>
2022-09-16 11:39:49 -0500

    Create Entity.gd

---
+++ Godot4/Entity.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+# Do not create an 'Entity' node.
+# Instead, inherit the 'Entity' class and override the methods defined below to add custom behavior.
+class_name Entity
+
+@export_enum("Collide", "Slide") var move_type := 0
+@export var health := 3
+
+var collision:KinematicCollision2D
+
+
+func _physics_process(delta):
+	_move(delta)
+	_animate(delta)
+	
+	match move_type:
+		0:
+			collision = move_and_collide(velocity * delta)
+		1:
+			move_and_slide()
+
+
+# Override to move your entity.
+# Directly modify the 'velocity' variable to do so.
+func _move(delta: float) -> void:
+	pass
+
+
+# Override to animate your entity.
+func _animate(delta: float) -> void:
+	pass
+
+
+# Override to create custom behavior upon taking damage.
+func take_damage() -> void:
+	if health > 1:
+		health -= 1
+	else:
+		die()
+
+
+# Override to create custom death behavior. (This is how you skip 'freeing' the object.)
+func die() -> void:
+	queue_free()
